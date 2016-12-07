68fe0f2b658eb67e12698e901ea441d606fe2229
machawt <mark@holt5.com>
2016-12-07 15:28:42 +1300

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
 function upz() {var name= prompt("Please enter your name", "");
            		 document.getElementById("line1").innerHTML =
         	"Hello " + name + "! How are you today?";};
+function downz() {var age=age-1;
+       		 document.getElementById("line2").innerHTML =
+        	"Age " + age ;};
 
 
 
