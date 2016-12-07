7aa214f1ebafc75f16a831ac5f450c4cdf4bff34
machawt <mark@holt5.com>
2016-12-07 15:18:32 +1300

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-function up() {var name= prompt("Please enter your name", "");
+function upz() {var name= prompt("Please enter your name", "");
            		 document.getElementById("line1").innerHTML =
         	"Hello " + name + "! How are you today?";};
 
 
-function down() {var age=age-1;
+function downz() {var age=age-1;
        		 document.getElementById("line2").innerHTML =
         	"Age " + age ;};
 
