8fb4fb61da41ad94f0c1eb58327202757cb84c53
machawt <mark@holt5.com>
2016-12-07 20:31:26 +1300

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,11 @@
 var up=0;
 var side=0;
 var row=["base","a", "b","c","d", "e","f","g", "h","j","k", "l","m","n", "o","p","q", "r","s","t", "u","v","w", "x","z"];
-var row[0]=["base","a", "b","c","d", "e","f","g", "h","j","k", "l","m","n", "o","p","q", "r","s","t", "u","v","w", "x","z"];
-var row[1]=["base","a", "b","c","d", "e","f","g", "h","j","k", "l","m","n", "o","p","q", "r","s","t", "u","v","w", "x","z"];
-function upz() {up=up+1;
+
        		 document.getElementById("line2").innerHTML =
         "First Letter" + up+"Second Letter"+side ;
-           
+               document.getElementById("line1").innerHTML =
+        "X" + row[up][side];
                };
 function downz() {up=up-1;
 document.getElementById("line1").innerHTML =
