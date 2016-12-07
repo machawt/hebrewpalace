404448abc197d9839fe004627509c5c7735446db
machawt <mark@holt5.com>
2016-12-07 20:28:00 +1300

    Update test.js

--- test.js
+++ test.js
@@ -6,8 +6,8 @@ var row[1]=["base","a", "b","c","d", "e","f","g", "h","j","k", "l","m","n", "o",
 function upz() {up=up+1;
        		 document.getElementById("line2").innerHTML =
         "First Letter" + up+"Second Letter"+side ;
-               document.getElementById("line1").innerHTML =
-        "X" + row[up][side];};
+           
+               };
 function downz() {up=up-1;
 document.getElementById("line1").innerHTML =
         "X" + row[up][side];
