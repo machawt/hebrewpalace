70f37c055f1653f5470f84e42fb779ad2f955bb4
machawt <mark@holt5.com>
2016-12-12 15:09:43 +1300

    Update test.js

--- test.js
+++ test.js
@@ -28,7 +28,7 @@ var heb=[["0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0
          ];
 
 
-function shw() {if  (flag==0) {flag=1;return} 
+function shw() {if  (flag==1) {flag=0;return} 
 else {flag=0;document.getElementById("show").innerHTML = " " + heb[up][side];}};
 
 function upz() {up=up+1;
